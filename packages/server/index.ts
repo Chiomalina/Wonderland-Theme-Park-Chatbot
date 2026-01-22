8f91df413bd62c789cd5f7d32c02314d3405d388
iamchioma <chiomalinaanaso@gmail.com>
2026-01-22 15:02:57 +0100

    manage API keys

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import type { Response, Request } from "express";
+import dotenv from "dotenv"
+
+dotenv.config()
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Anotate types in home route
 app.get("/", (req: Request, res: Response) => {
-    res.send("Hello Nigeria!");
+    res.send("Hello World!");
 });
 
 app.listen(port, () => {
